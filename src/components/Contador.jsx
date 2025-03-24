9df85c395c037e5d5c895acca9bb19a130462bd5
Kaua <rodrigueskaua76@gmail.com>
2025-03-24 11:19:06 -0300

    contador funcionando

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,19 @@
+import {useState} from 'react'
 import React from 'react'
 
 const Contador = () => {
+
+    //HOOKS-useState - Manipula o estado da variavel
+    const [contador, setContador]=useState(0);
+
   return (
     <>
       <h1>meu contador</h1>
-      
+
+      <p>Você Clicou: {contador}</p>
+      <button onClick={()=>setContador(contador +1)}>Aumentar</button>
+      <button onClick={()=>setContador(contador -1)}>Diminuir</button>
+
     </>
   )
 }
